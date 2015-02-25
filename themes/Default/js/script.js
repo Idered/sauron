e79b5c7de392ae76664114f37984acbc81206467
Kasper Mikiewicz <IderedPL@gmail.com>
2015-02-25 20:56:20 +0100

    Add some logic to default js file

--- themes/Default/js/script.js
+++ themes/Default/js/script.js
@@ -1,3 +1,19 @@
 (function ( $ ) {
 
+    function Theme() {
+        // Your options goes here
+        this.foo = false;
+
+        // Kickstart your methods
+        this.nav();
+    }
+
+    Theme.prototype.nav = function () {
+        // Here you have access to this.foo
+    };
+
+    $( document ).ready( function () {
+        new Theme();
+    } );
+
 })( jQuery );
